95779acbbed70e8c32d1cf5869551ed4f541c96e
Wataru.Kasahara <kasahara1019@gmail.com>
2020-06-17 20:11:28 +0900

    feat(todos): Use the UI Library

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UiModule } from '@nx-tutorial/ui';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, UiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
